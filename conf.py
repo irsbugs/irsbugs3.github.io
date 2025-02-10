8e2b40df5308bc04acefc43f37b7eb6a32e2d86f
Ian Stewart <irsbugs@hotmail.com>
2025-02-10 22:24:56 +1300

    Update conf.py

--- conf.py
+++ conf.py
@@ -296,7 +296,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog3"
+THEME = "bootblog4"
 
 # A theme color. In default themes, it might be displayed by some browsers as
 # the browser UI color (eg. Chrome on Android). Other themes might also use it
