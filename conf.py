6264984994b03db4b748d66d7bee74db8b3d1f67
Ian Stewart <irsbugs@hotmail.com>
2025-02-13 09:21:38 +1300

    Update conf.py

--- conf.py
+++ conf.py
@@ -286,7 +286,7 @@ NAVIGATION_LINKS = {
 NAVIGATION_ALT_LINKS = {
     DEFAULT_LANG: (
         ("/index.html", "Welcome"),
-        ("/legal/", "Legal"),
+        ("/legal-introduction/", "Legal"),
         ("/legal1/", "Legal1"),        
         ("/streisand/", "Streisand"),  
         ("/feedback/", "Feedback"),
